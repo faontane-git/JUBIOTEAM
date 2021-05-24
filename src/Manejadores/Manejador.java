f1573246eee13caaff31045a625a5e7a1f9677c6
faontane-git <73259474+faontane-git@users.noreply.github.com>
2021-05-23 19:31:07 -0500

    Proyecto Final

---
+++ src/Manejadores/Manejador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Manejadores;
+
+import Ventanas.HormigolandiaQuiz2;
+
+/**
+ *
+ * @author fabri
+ */
+public class Manejador {
+
+    public static boolean mundo1 = true;
+    public static boolean mundo2 = false;
+    public static boolean mundo3 = false;
+    public static boolean respuesta = false;
+    public static int aciertos=0;
+    public static int contador = 1;
+ 
+
+}
